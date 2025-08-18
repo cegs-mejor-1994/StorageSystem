d091e5eded0c0f987c2cdfe0f5bbb68080593842
Ernesto Gonzalez Salgado <cegslevono1994@gmail.com>
2025-08-18 18:24:56 -0500

    Fixing the ProductCreate

--- StorageSystem/StorageSystem.WEB/Pages/Products/ProductCreate.razor.cs
+++ StorageSystem/StorageSystem.WEB/Pages/Products/ProductCreate.razor.cs
@@ -9,6 +9,7 @@ namespace StorageSystem.WEB.Pages.Products
     {
         private Product product = new();   
         private List<int> ListIdsReferences { get; set; } = new();
+        private string ListReferencesNames { get; set; } = string.Empty;
         private List<Reference> references { get; set; } = new();        
         private string productName { get; set; } = string.Empty;
         private string productCode { get; set; } = string.Empty;
@@ -32,44 +33,65 @@ namespace StorageSystem.WEB.Pages.Products
         }
 
         private async Task CreateAsync()
-        {
-            var result = await sweetAlertService.FireAsync(new SweetAlertOptions
-            {
-                Title = "Confirmacion",
-                Text = $"¿Estas seguro que quieres crear el product: {productName} con su codigo respectivo: {productCode}?",
-                Icon = SweetAlertIcon.Question,
-                ShowCancelButton = true,
-            });
-
+        {           
             try
             {
-                if (ListIdsReferences.Count > 0)
+                if (!string.IsNullOrEmpty(productCode) && !string.IsNullOrWhiteSpace(productName))
                 {
-                    foreach (var referenceId in ListIdsReferences)
+                    if (ListIdsReferences.Count > 0)
                     {
-                        product.Code = productCode + GetReferenceName(referenceId);
-                        product.Name = productName + " " + GetMeasurementAndReferenceName(referenceId);
-                        product.ReferenceId = referenceId;
-                        var responseHttp = await repository.PostAsync("/api/Products", product);
-                        if (responseHttp.Error)
+                        foreach (var referenceId in ListIdsReferences)
+                        {                           
+                            ListReferencesNames += productName + " " + GetMeasurementAndReferenceName(referenceId) + ", ";
+                        }
+                        ListReferencesNames = ListReferencesNames.TrimEnd(',', ' ');
+
+                        var result = await sweetAlertService.FireAsync(new SweetAlertOptions
                         {
-                            var message = await responseHttp.GetErrorMessageAsync();
-                            await sweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                            Title = "Confirmacion",
+                            Text = $"¿Estas seguro que quieres crear los siguientes productos: {ListReferencesNames}?",
+                            Icon = SweetAlertIcon.Question,
+                            ShowCancelButton = true,
+                        });
+
+                        var confirm = string.IsNullOrEmpty(result.Value);
+                        if (confirm)
+                        {
                             return;
-                        }                              
+                        }
+
+                        foreach (var referenceId in ListIdsReferences)
+                        {
+                            product.Code = productCode + GetReferenceName(referenceId);
+                            product.Name = productName + " " + GetMeasurementAndReferenceName(referenceId);
+                            product.ReferenceId = referenceId;
+                            var responseHttp = await repository.PostAsync("/api/Products", product);
+                            if (responseHttp.Error)
+                            {
+                                var message = await responseHttp.GetErrorMessageAsync();
+                                await sweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                                return;
+                            }
+                        }
+                        navigationManager.NavigateTo("/products");
+                        var toast = sweetAlertService.Mixin(new SweetAlertOptions
+                        {
+                            Toast = true,
+                            Position = SweetAlertPosition.BottomEnd,
+                            ShowConfirmButton = true,
+                            Timer = 3000
+                        });
+                        await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro creado con éxito.");
                     }
-                    navigationManager.NavigateTo("/products");
-                    var toast = sweetAlertService.Mixin(new SweetAlertOptions
+                    else
                     {
-                        Toast = true,
-                        Position = SweetAlertPosition.BottomEnd,
-                        ShowConfirmButton = true,
-                        Timer = 3000
-                    });
-                    await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro creado con éxito.");
-                } else
+                        await sweetAlertService.FireAsync("Error", "Debes elegir por lo menos una referencia", SweetAlertIcon.Error);
+                    }
+                }
+                else
                 {
-                    await sweetAlertService.FireAsync("Error", "Debes elegir por lo menos una referencia", SweetAlertIcon.Error);
+                    await sweetAlertService.FireAsync("Error", "El nombre y el código del producto son obligatorios.", SweetAlertIcon.Error);
+                    return;
                 }
             }
             catch (Exception ex)
